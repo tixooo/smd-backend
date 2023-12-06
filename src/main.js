7426ed84a26a93f1488dd07fbb715da44cf1561f
Stefanov <konstantin.stefanov@dxc.com>
2023-12-06 16:35:47 +0200

    test

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import express from 'express';
 const main = express();
 
 main.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log('Server is listening on port - 3000');
 })
