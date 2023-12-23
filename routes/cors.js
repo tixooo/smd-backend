bcbfdce068b971a69400290e5878e3be2dc3c28e
Stefanov <konstantin.stefanov@dxc.com>
2023-12-23 23:45:03 +0200

    added cors options for localhost

--- routes/cors.js
+++ routes/cors.js
@@ -1,7 +1,25 @@
+// const corsOptions = {
+//     origin: "https://smdashboard-tixooo-ec2b5662f2a102425c47790ccd84940098ce289abd2b.gitlab.io",
+// methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     optionsSuccessStatus: 204
+// }
+// export default corsOptions;
+
+const allowedOrigins = [
+    "https://smdashboard-tixooo-ec2b5662f2a102425c47790ccd84940098ce289abd2b.gitlab.io",
+    "http://localhost:8080",
+];
+
 const corsOptions = {
-    origin: "https://smdashboard-tixooo-ec2b5662f2a102425c47790ccd84940098ce289abd2b.gitlab.io",
-methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    optionsSuccessStatus: 204
-}
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 204,
+};
 
 export default corsOptions;
