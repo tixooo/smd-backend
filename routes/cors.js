d5350d7cee8745a0a27418c9bcd6c6bbd436edce
Stefanov <konstantin.stefanov@dxc.com>
2023-12-23 23:18:56 +0200

    added few changes

--- routes/cors.js
+++ routes/cors.js
@@ -1,7 +1,6 @@
 const corsOptions = {
     origin: "https://smdashboard-tixooo-ec2b5662f2a102425c47790ccd84940098ce289abd2b.gitlab.io",
 methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: false,
     optionsSuccessStatus: 204
 }
 
