4bd1d9813fb4e61a8a99dc3cfcc1aec53f2f7fda
Stefanov <konstantin.stefanov@dxc.com>
2023-12-26 15:48:43 +0200

    removed the db connection options

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.DB_CONNECTION, {dbName: "SMD-main"})
+        const conn = await mongoose.connect(process.env.DB_CONNECTION)
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
         console.error(`Error: ${err.message}`);
