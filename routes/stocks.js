a2cdb3379e5d5f2a52fdf8b5e73cede2f97db058
Stefanov <konstantin.stefanov@dxc.com>
2024-01-04 00:32:19 +0200

    changing things around

--- routes/stocks.js
+++ routes/stocks.js
@@ -19,9 +19,9 @@ router.post('/addStock', async (req, res) => {
 router.get('/stocks', async (req, res) => {
     try {
         // TODO to write validations
-        const stocks = await Stock.find({})
-        console.log('Retrieved stocks:', stocks);
-        res.status(200).json({message: 'Stocks retrieved successfully', stocks: stocks});
+        const allStocks = await Stock.find({})
+        console.log('Retrieved stocks:', allStocks);
+        res.status(200).json({message: 'Stocks retrieved successfully', allStocks: allStocks});
     } catch (error) {
         console.error('Error fetching stocks:', error);
         res.status(500).json({ message: 'Internal Server Error'});
