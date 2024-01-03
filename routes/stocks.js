3049b0fc1f64342d9aae99e4a690ca15377b768b
Stefanov <konstantin.stefanov@dxc.com>
2024-01-04 00:24:43 +0200

    added some console logs for stocks

--- routes/stocks.js
+++ routes/stocks.js
@@ -19,10 +19,11 @@ router.post('/addStock', async (req, res) => {
 router.get('/stocks', async (req, res) => {
     try {
         // TODO to write validations
-        const stock = await Stock.find({})
-        res.status(200).json({message: 'Stocks retrieved successfully', stocks: stock});
+        const stocks = await Stock.find({})
+        console.log('Retrieved stocks:', stocks);
+        res.status(200).json({message: 'Stocks retrieved successfully', stocks: stocks});
     } catch (error) {
-        console.error(error);
+        console.error('Error fetching stocks:', error);
         res.status(500).json({ message: 'Internal Server Error'});
     }
 });
