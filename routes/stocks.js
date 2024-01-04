d7cbc6c552d2df9deeaa10da8045be77fde87bf8
Stefanov <konstantin.stefanov@dxc.com>
2024-01-04 22:02:11 +0200

    added stocks

--- routes/stocks.js
+++ routes/stocks.js
@@ -3,16 +3,27 @@ const router = express.Router();
 import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import Stock from '../models/Stock.js';
+import dotenv from 'dotenv';
 
 
-router.post('/addStock', async (req, res) => {
+router.get('/addStock', async (err, res, data) => {
     try {
-        // TODO to write validations
-        const newSymbol = new Stock(req.body);
-        await newSymbol.save();
-        res.status(201).json({message: 'Stock added successfully'})
+        // write a connection to the API from .env
+        const API_URL = process.env.API_URL;
+        // we need to connect to that URL
+        const response = await fetch(API_URL);
+        if (err) {
+            console.log('Error:', err);
+        } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+        } else {
+            // data is successfully parsed as a JSON object:
+            console.log(data);
+            console.log(response)
+            res.status(200).json({message: 'Stocks retrieved successfully', allStocks: data});
+        }
     } catch (error) {
-        res.status(500).json({ message: 'Internal Server Error' });
+        res.status(500).json({ message: 'Internal Server Error'});
     }
 });
 
