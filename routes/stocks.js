19e58e144ee01c8b98d4604573d9f1a4358cff5f
Stefanov <konstantin.stefanov@dxc.com>
2024-01-14 17:33:02 +0200

    test

--- routes/stocks.js
+++ routes/stocks.js
@@ -6,40 +6,40 @@ import Stock from '../models/Stock.js';
 import AllStocks from '../models/AllStocks.js';
 import dotenv from 'dotenv';
 
-// router.get('/addStock', async (req, res) => {
-//     try {
-//
-//         const stockData = await AllStocks.findOne({});
-//
-//
-//         if (stockData) {
-//             res.json(stockData);
-//             console.log(stockData)
-//         } else {
-//             res.status(404).send('Stock not found');
-//             console.log(stockData)
-//         }
-//     } catch (e) {
-//         res.status(500).send(e.message);
-//     }
-// });
-
 router.get('/addStock', async (req, res) => {
     try {
-        const { symbol } = req.query;
-        if (!symbol) {
-            return res.status(400).send('Symbol parameter is required');
-        }
-        const stockData = await AllStocks.findOne({"MetaData.2. Symbol": symbol});
+
+        const stockData = await AllStocks.findOne({});
+
+
         if (stockData) {
             res.json(stockData);
+            console.log(stockData)
         } else {
             res.status(404).send('Stock not found');
+            console.log(stockData)
         }
     } catch (e) {
         res.status(500).send(e.message);
     }
 });
+
+// router.get('/addStock', async (req, res) => {
+//     try {
+//         const { symbol } = req.query;
+//         if (!symbol) {
+//             return res.status(400).send('Symbol parameter is required');
+//         }
+//         const stockData = await AllStocks.findOne({"MetaData.2. Symbol": symbol});
+//         if (stockData) {
+//             res.json(stockData);
+//         } else {
+//             res.status(404).send('Stock not found');
+//         }
+//     } catch (e) {
+//         res.status(500).send(e.message);
+//     }
+// });
 
 router.get('/stocks', async (req, res) => {
     try {
